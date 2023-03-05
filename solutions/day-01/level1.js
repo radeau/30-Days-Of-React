7c02efe07128586124e219361d5ed46ef9c041b1
kevinradp <kevinradp@gmail.com>
2023-03-05 17:37:18 +0400

    update solution

---
+++ solutions/day-01/level1.js
@@ -0,0 +1,51 @@
+//EXERCISE 1
+// Declare an empty array;
+const arr = Array();
+
+// Declare an array with more than 5 number of elements
+const arr1 = Array(6).fill('z');
+
+// Find the length of your array
+console.log(arr1.length);
+
+// Get the first item, the middle item and the last item of the array
+const newArr =[2,3,5,0,5,4,8]
+console.log(newArr[0],newArr[Math.floor(newArr.length/2)],newArr[newArr.length-1])
+
+// Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+const mixedDataTypes = [1,2,3,4,5,6,7,8,9,10,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+console.log(mixedDataTypes.length)
+
+// Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+const itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+// Print the array using console.log()
+console.log(itCompanies);
+
+// Print the number of companies in the array
+console.log(itCompanies.length);
+
+// Print the first company, middle and last company
+console.log(itCompanies[0],itCompanies[Math.floor(itCompanies.length/2)],itCompanies[itCompanies.length-1])
+
+// Print out each company using map()
+itCompanies.map((company)=>console.log(company));
+
+
+// Change each company name to uppercase one by one and print them out
+itCompanies.map((company)=>console.log(company.toUpperCase()))
+
+// Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+
+
+// Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
+// Filter out companies which have more than one 'o' without the filter method
+// Sort the array using sort() method
+// Reverse the array using reverse() method
+// Slice out the first 3 companies from the array
+// Slice out the last 3 companies from the array
+// Slice out the middle IT company or companies from the array
+// Remove the first IT company from the array
+// Remove the middle IT company or companies from the array
+// Remove the last IT company from the array
+// Remove all IT companies
